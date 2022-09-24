6471f7060364905f3f6f45e5a946e86d5186dd7d
Bryan Peabody <hi@bryanpeabody.com>
2022-09-24 17:28:50 -0400

    Updated debug statements.

--- cubsnewsbot.py
+++ cubsnewsbot.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta
 import requests
 import os
-from sys import argv
 
 #
 # Removes previous days sent messages file.
@@ -9,31 +8,28 @@ from sys import argv
 def clean_up_files():
     day = datetime.today() - timedelta(days=1)
     filename = "./txt/" + day.strftime("%Y-%m-%d") + ".txt"
-    
+
     try:
         os.remove(filename)
     except:
         pass
-
 #
 # Checks if a passed in text exists in the sent messages file.
 #
-def does_exist_in_sent_messages(whathappened):   
-    clean_up_files()
-
-    filename = "./txt/" + datetime.now().strftime("%Y-%m-%d") + ".txt"    
+def does_exist_in_sent_messages(whathappened):
+    filename = "./txt/" + datetime.now().strftime("%Y-%m-%d") + ".txt"
 
     with open(filename,'r') as f:
-        if whathappened in f.read():            
+        if whathappened in f.read():
             return True
 
-        return False
+    return False
 
 #
 # Updates the sent messages file.
 #
 def update_sent_messages(whathappened):
-    filename = "./txt/" + datetime.now().strftime("%Y-%m-%d") + ".txt"    
+    filename = "./txt/" + datetime.now().strftime("%Y-%m-%d") + ".txt"
 
     with open(filename,'a+') as f:
         f.writelines(whathappened + "\n")
@@ -46,7 +42,7 @@ def check_mlb_api(pushover_token, pushover_user_id):
     today = datetime.now().strftime("%Y-%m-%d")
 
     # Define the API endpoint
-    mlb_api = "https://statsapi.mlb.com/api/v1/transactions?teamId=112&startDate=" + today + "&endDate=" + today    
+    mlb_api = "https://statsapi.mlb.com/api/v1/transactions?teamId=112&startDate=" + today + "&endDate=" + today
     pushover_api = "https://api.pushover.net/1/messages.json?token=" + pushover_token + "&user=" + pushover_user_id + "&message="
 
     # Call MLB API
@@ -66,22 +62,23 @@ def check_mlb_api(pushover_token, pushover_user_id):
 
             # If we haven't sent this one before, send it
             if not does_exist_in_sent_messages(whathappened):
-                pass
                 pushover_response = requests.post(pushover_api + whathappened)
-                
+
                 if pushover_response.status_code == 200:
                     update_sent_messages(whathappened)
 
 #
 # Entry point
 #
-if __name__ == "__main__":   
-    # Get environment varibles.
-    pushover_token = os.getenv('PUSHOVER_TOKEN')
-    pushover_user_id = os.getenv('PUSHOVER_USER_ID')
+# Cleanup previous day's file.
+clean_up_files()
 
-    # If the environment varibles are not set, show an error.
-    if pushover_token is not None and pushover_user_id is not None:
-        check_mlb_api(pushover_token, pushover_user_id)
-    else:
-        print("ERROR! Environment varibles are not set!")
+# Get environment varibles.
+pushover_token = os.getenv('PUSHOVER_TOKEN')
+pushover_user_id = os.getenv('PUSHOVER_USER_ID')
+
+# If the environment varibles are not set, show an error.
+if pushover_token is not None and pushover_user_id is not None:
+    check_mlb_api(pushover_token, pushover_user_id)
+else:
+    print("ERROR! Environment varibles are not set!")
